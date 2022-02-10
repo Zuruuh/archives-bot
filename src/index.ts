dde6bce5501aa3ee08f728652993956490243b32
Younès Ziadi <ziadi.mail.pro@gmail.com>
2022-02-11 00:40:03 +0100

    🐛 Resolved client login promise

--- src/index.ts
+++ src/index.ts
@@ -7,10 +7,9 @@ async function main(): Promise<void> {
   configDotenv({ path: resolve(__dirname, '..', 'config', 'envs') });
 
   const client = new Client({ intents: Object.values(Intents.FLAGS) });
-
   await Handlers.setup(client);
 
-  client.login(process.env.BOT_TOKEN);
+  await client.login(process.env.BOT_TOKEN);
 }
 
 main().catch((e: Error) => console.error(e));
