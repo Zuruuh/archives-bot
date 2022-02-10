d54c7bf4466dc990e16ce30d3f51ba3949a24e30
Younès Ziadi <ziadi.mail.pro@gmail.com>
2022-02-10 20:29:33 +0100

    ✨ Updated run method to be compatible with arguments module

--- src/commands/$Command.ts
+++ src/commands/$Command.ts
@@ -1,13 +1,17 @@
-import { Client, Message } from 'discord.js';
+import { Client, Message, PermissionString } from 'discord.js';
 import { ICommand } from '../types';
 
 export abstract class $Command implements ICommand {
-  public abstract aliases: string[];
+  public abstract readonly aliases: string[];
+  public abstract readonly permissions: PermissionString[];
 
   // eslint-disable-next-line
   // @ts-ignore
   // TODO: Add typeorm entity manager to constructor
   constructor(private readonly client: Client) {}
 
-  public abstract run(message: Message, ...args: string[]): Promise<void>;
+  public abstract run<T extends any[]>(
+    message: Message,
+    args: T
+  ): Promise<void>;
 }
