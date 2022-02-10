e87395f9f91e54b944bf7cee89fec179c6ab74eb
Younès Ziadi <ziadi.mail.pro@gmail.com>
2022-02-10 14:15:50 +0100

    🏷️ Created interface for commands

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,11 @@
+import { Message } from 'discord.js';
+
 export type DiscordEventRecord = Record<
   string,
   (...args: any) => Promise<void>
 >;
+
+export interface ICommand {
+  run: (message: Message, ...args: string[]) => Promise<void>;
+  aliases: string[];
+}
